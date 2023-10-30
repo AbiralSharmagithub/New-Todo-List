45b52108d43fc4e33f6d4a5501f55a9789514cc3
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-30 17:50:07 +0545

    refactor:changes some code using props

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a="Aashish";
+const b="Aavash";
+const c="Dipace";
+const d="Abiral";
+
+export default a;
+export {b};
+export {c};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import abk,{b,c,d} from './module1.mjs'
+console.log(abk);
+console.log(b);
+console.log(c);
+console.log(d);
