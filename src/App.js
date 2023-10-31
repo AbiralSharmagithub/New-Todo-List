748569b41483c03cddb5f19a1b6c581b7a4186f9
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-31 16:40:12 +0545

    fix:Make a to-do list project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import Navbar from  './component/Navbar.js';
-function App() {
+import React, { useState } from 'react'
+import './App.css'
+export default function App() {
+  const[inputList,setInputList]= useState("");
+  const itemEvent = (event)=> {
+    setInputList(event.target.value);
+  }
   return (
     <>
- <Navbar title="TextUtils" about="About Us"/>
-</>
-  );
+      <div className='main_div'>
+       <div className='centre_div'>
+        <h1 className='to-do'>ToDo List</h1>
+        <input type="text" placeholder='Enter name of items' className='items' onChange={itemEvent}/>
+        <button className='button'>+</button>
+        <ol>
+          <li>{inputList}</li>
+        </ol>
+       </div>
+      </div>
+    </>
+  )
 }
-
-export default App;
